d2b1b7a0f2cd88e1cd78a24d6c964b974a884752
wkym461a <wkym461a@gmail.com>
2024-10-18 07:01:53 +0900

    [Add] useOrientation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { mediaQuery, useMediaQuery } from './useMediaQuery';
+import { useOrientation } from './useOrientation';
 import './App.css';
 
 function App() {
 	const isSp = useMediaQuery(mediaQuery.sp);
 	const isTablet = useMediaQuery(mediaQuery.tablet);
 	const isPc = useMediaQuery(mediaQuery.pc);
+
+	const orientation = useOrientation();
 
   return (
     <>
@@ -20,6 +23,7 @@ function App() {
 			:
 			<p>Viewing on Unknown Device.</p>
 		}
+		<p>{`The orientation of the screen is: ${orientation.type}`}</p>
     </>
   )
 }
